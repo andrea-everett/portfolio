3f74573ee93317866adab51d550e223ed7537041
andrea-everett <andrea.dawn.everett@gmail.com>
2022-05-04 17:29:00 -0400

    first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services_modal'),
+                modalBtns = document.querySelectorAll('.services_button'),
+                modalClose = document.querySelectorAll('.services_modal-close')
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+    mb.addEventListener('click', () =>{
+            modal(i)
+    })
+})
+
+modalClose.forEach((mc) =>{
+    mc.addEventListener('click', () =>{
+        modalViews.forEach((mv) =>{
+            mv.classList.remove('active-modal')
+        })
+    })
+}
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work_container', {
+    selectors: {
+        target: '.work_card'
+    },
+    animation: {
+        duration: 300
+    }
+})
+
+/* Link active work */ 
+const linkWork = dcoument.querySelectorAll('.work_item')
+
+function activeWork(){
+    linkWork.forEach(L => L.classList.remove('.acitve-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(L=> l.add('click', activeWork))
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections =document.querySelectorAll('[]')
+
+function scrollActive(){
+    const scrollY =window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+                        sectionTop = current.offsetTop -58,
+                        sectionID = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== LIGHT DARK THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme ='bx-sun'
+
+const selectedTheme = localStorage.getItme('selected-theme')
+const selectedicon = localStorage.getItme('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add': 'remove' ](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+/*=============== SCROLL REVEAL ANIMATION ===============*/
