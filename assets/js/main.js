19f31767f173052715d7580505334e0117a38947
andrea-everett <andrea.dawn.everett@gmail.com>
2022-05-13 10:27:35 -0400

    mobile-edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@ let mixerPortfolio = mixitup('.work_container', {
 })
 
 /* Link active work */ 
-const linkWork = dcoument.querySelectorAll('.work_item')
+const linkWork = document.querySelectorAll('.work_item')
 
 function activeWork(){
     linkWork.forEach(L => L.classList.remove('.acitve-work'))
@@ -50,31 +50,24 @@ linkWork.forEach(L=> l.add('click', activeWork))
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections =document.querySelectorAll('[]')
+function openWindow() {
+    window.open("https://andrea-everett.github.io/sunnyside-challenge/");
+  }
 
-function scrollActive(){
-    const scrollY =window.pageYOffset
+  function openWindow1() {
+    window.open("https://andrea-everett.github.io/Article-Preview/");
+  }
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-                        sectionTop = current.offsetTop -58,
-                        sectionID = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+  function openWindow2() {
+    window.open("https://andrea-everett.github.io/joke-teller/");
+  }
 
 /*=============== LIGHT DARK THEME ===============*/ 
 const themeButton = document.getElementById('theme-button')
 const lightTheme = 'light-theme'
-const iconTheme ='bx bx-sun'
+const iconTheme =' bx-sun'
 
-const selectedTheme = localStorage.getIItem('selected-theme')
+const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
 const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
@@ -92,4 +85,28 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== OPEN GMAIL SIGNIN ===============*/
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('customBtn'));
+  });
+};
+
+function attachSignin(element) {
+  console.log(element.id);
+  auth2.attachClickHandler(element, {},
+      function(googleUser) {
+        document.getElementById('name').innerText = "Signed in: " +
+            googleUser.getBasicProfile().getName();
+      }, function(error) {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+}
